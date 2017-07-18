aa8729e7f9536fc0c52feac43c77b86c06bacd33
Steven Galvin <steven.john.galvin@gmail.com>
2017-07-18 15:30:34 -0700

    add paperclip with default image to each product as well as display each image with product details. add logic to display total items in cart as opposed to length of order items.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   has_many :order_items
 
+  has_attached_file :image, default_url: "/images/placeholder.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   validates :name, :description, :price, presence: true
   validates :description, length: { maximum: 300 }
 end
